b59dec1308422ae4fc301a88f0abed7b527d56c1
Jerome Dane <jerome.dane@gmail.com>
2021-10-20 19:50:56 -0400

    Brag about rotation animation using emojis

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>☝️ Check out the cool rotation animation! 💪</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
